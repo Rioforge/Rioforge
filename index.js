1bd22672067c3f2b6e1e175bee2f8d9fd9969447
rioForge <100526505+Rioforge@users.noreply.github.com>
2022-03-01 17:04:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
         const progress = document.querySelector('.progress')
         const percentage = document.querySelector('.progress span')
 
-        let per = 0;
+        let per = 70;
         function progressLoad(){
             if(per>=80){
                 progress.style.width = '80%';
-                percentage.innerHTML = "80%"
+                percentage.innerHTML = "80%";
 
             }else{
                 progress.style.width = '${per}%';
